4daf4c26f7c6ca0c404c42526e5842a4c99c9125
Florian Valence <florian.m.valence@gmail.com>
2016-05-10 15:19:13 +0200

    add license

--- dist/templates/ftv.components.cnil.js
+++ dist/templates/ftv.components.cnil.js
@@ -1,1 +1,21 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 France Télévisions
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
+ * to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+ * the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ * THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 angular.module("ftv.components.cnil.templates", []).run(["$templateCache", function($templateCache) {$templateCache.put("ftv.components.cnil.html","<div class=\"ftvCnil\" ng-if=\"show\"><span>En poursuivant votre navigation sur ce site, vous acceptez l\'utilisation de cookies à des fins statistiques, publicitaires et de personnalisation.</span> <a href=\"{{link}}\" target=\"_blank\" class=\"ftvCnil__link ftvCnil__link__desktop\">Pour en savoir plus et paramétrer les cookies</a> <a href=\"{{link}}\" target=\"_blank\" class=\"ftvCnil__link ftvCnil__link__mobile\">En savoir plus</a><div class=\"ftvCnil__close\" ng-click=\"close()\"><span class=\"ftvCnil__close__text\">X</span></div></div>");}]);
